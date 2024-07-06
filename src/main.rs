5409e5c6fcd9738ae086e2d6c741df1f43521c0d
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-07-06 06:19:38 -0400

    Add license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright © 2024 Jaxydog
+//
+// This file is part of rs.
+//
+// rs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+// General Public License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// rs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+// General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with rs. If not,
+// see <https://www.gnu.org/licenses/>.
+
 use std::cmp::Ordering;
 use std::env::{args, current_dir};
 use std::fmt::Display;
