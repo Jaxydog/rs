a752c243b936d861f19fa9d0444495d8143e66fb
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-07-06 18:51:17 -0400

    Fix copyright header

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 #
 # Copyright © 2024 Jaxydog
 #
-# This file is part of Scripts.
+# This file is part of rs.
 #
-# Scripts is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# rs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 #
-# Scripts is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+# rs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License along with Scripts. If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License along with rs. If not, see <https://www.gnu.org/licenses/>.
 
 if [ -d ./target/ ]; then
     echo 'Cleaning up target directory.'
