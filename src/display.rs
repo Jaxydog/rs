c8fccfb1da05b90c0cfeb2f869f6b62ec5827040
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-07-08 04:33:51 -0400

    Improve documentation, add character colors for Windows

--- src/display.rs
+++ src/display.rs
@@ -275,13 +275,27 @@ impl Permissions {
     /// This function will return an error if the permission could not be displayed.
     fn show_char<W: Write>(&self, f: &mut W, character: char) -> Result<()> {
         match character {
+            // No value.
             c @ '-' => cwrite!(bright_black; f, "{c}"),
+            // Read (Unix) / Read-only (Windows).
             c @ 'r' => cwrite!(bright_yellow; f, "{c}"),
+            // Write (Unix).
             c @ 'w' => cwrite!(bright_red; f, "{c}"),
+            // Executable (Unix).
             c @ 'x' => cwrite!(bright_green; f, "{c}"),
+            // Archive (Windows).
+            c @ 'a' => cwrite!(bright_red; f, "{c}"),
+            // Hidden (Windows).
+            c @ 'h' => cwrite!(bright_purple; f, "{c}"),
+            // System (Windows).
+            c @ 's' => cwrite!(bright_green; f, "{c}"),
+            // Directory.
             c @ 'd' => cwrite!(bright_blue; f, "{c}"),
+            // Symlink.
             c @ 'l' => cwrite!(bright_cyan; f, "{c}"),
+            // File.
             c @ '.' => cwrite!(white; f, "{c}"),
+            // Anything else.
             unknown => cwrite!(bright_magenta; f, "{unknown}"),
         }
         .map_err(Into::into)
