0dc036713e1ed100f37d597b019530cf7efb9c73
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-01-06 09:56:24 -0500

    Properly implement unix file type display

--- src/display/permissions.rs
+++ src/display/permissions.rs
@@ -46,8 +46,30 @@ impl<'ar> PermissionsDisplay<'ar> {
     /// This function will return an error if the permissions could not be displayed.
     #[cfg(target_family = "unix")]
     fn show_entry<W: Write>(&self, f: &mut W, entry: &Entry) -> Result<()> {
+        const FILE_TYPE_MASK: u32 = 0o0_170_000;
+        const SOCKET: u32 = 0o0_140_000;
+        const SYMBOLIC_LINK: u32 = 0o0_120_000;
+        const FILE: u32 = 0o0_100_000;
+        const BLOCK_DEVICE: u32 = 0o0_060_000;
+        const DIRECTORY: u32 = 0o0_040_000;
+        const CHARACTER_DEVICE: u32 = 0o0_020_000;
+        const FIFO_PIPE: u32 = 0o0_010_000;
+
         let mode = <Metadata as std::os::unix::fs::MetadataExt>::mode(&entry.data);
         let string = ::umask::Mode::from(mode).to_string();
+
+        let character = match mode & FILE_TYPE_MASK {
+            SOCKET => 's',
+            SYMBOLIC_LINK => 'l',
+            FILE => '-',
+            BLOCK_DEVICE => 'b',
+            DIRECTORY => 'd',
+            CHARACTER_DEVICE => 'c',
+            FIFO_PIPE => 'p',
+            _ => '?',
+        };
+
+        self.show_char(f, character)?;
 
         for character in string.chars() {
             self.show_char(f, character)?;
@@ -66,6 +88,14 @@ impl<'ar> PermissionsDisplay<'ar> {
         let bits = <Metadata as std::os::windows::fs::MetadataExt>::file_attributes(&entry.data);
         let string = WindowsPermissions { bits }.to_string();
 
+        if entry.data.is_symlink() {
+            self.show_char(f, 'l')?;
+        } else if entry.data.is_dir() {
+            self.show_char(f, 'd')?;
+        } else {
+            self.show_char(f, '-')?;
+        }
+
         for character in string.chars() {
             self.show_char(f, character)?;
         }
@@ -78,28 +108,56 @@ impl<'ar> PermissionsDisplay<'ar> {
     /// # Errors
     ///
     /// This function will return an error if the permission could not be displayed.
+    #[cfg(target_family = "unix")]
     fn show_char<W: Write>(&self, f: &mut W, character: char) -> Result<()> {
         match character {
-            // No value.
-            c @ '-' => cwrite!(self, bright_black; f, "{c}"),
-            // Read (Unix) / Read-only (Windows).
+            // Read permission.
             c @ 'r' => cwrite!(self, bright_yellow; f, "{c}"),
-            // Write (Unix).
+            // Write permission.
             c @ 'w' => cwrite!(self, bright_red; f, "{c}"),
-            // Executable (Unix).
+            // Execute permission.
             c @ 'x' => cwrite!(self, bright_green; f, "{c}"),
-            // Archive (Windows).
-            c @ 'a' => cwrite!(self, bright_red; f, "{c}"),
-            // Hidden (Windows).
-            c @ 'h' => cwrite!(self, bright_purple; f, "{c}"),
-            // System (Windows).
-            c @ 's' => cwrite!(self, bright_green; f, "{c}"),
+            // File / No value.
+            c @ '-' => cwrite!(self, bright_black; f, "{c}"),
             // Directory.
             c @ 'd' => cwrite!(self, bright_blue; f, "{c}"),
             // Symbolic link.
             c @ 'l' => cwrite!(self, bright_cyan; f, "{c}"),
-            // File.
-            c @ '.' => cwrite!(self, white; f, "{c}"),
+            // Socket.
+            c @ 's' => cwrite!(self, bright_green; f, "{c}"),
+            // Block device.
+            c @ 'b' => cwrite!(self, bright_red; f, "{c}"),
+            // Character device.
+            c @ 'c' => cwrite!(self, bright_yellow; f, "{c}"),
+            // FIFO pipe.
+            c @ 'p' => cwrite!(self, bright_purple; f, "{c}"),
+            // Anything else.
+            unknown => cwrite!(self, bright_magenta; f, "{unknown}"),
+        }
+    }
+
+    /// Displays an entry's permission character.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the permission could not be displayed.
+    #[cfg(target_family = "windows")]
+    fn show_char<W: Write>(&self, f: &mut W, character: char) -> Result<()> {
+        match character {
+            // Read-only.
+            c @ 'r' => cwrite!(self, bright_yellow; f, "{c}"),
+            // Archive.
+            c @ 'a' => cwrite!(self, bright_red; f, "{c}"),
+            // Hidden.
+            c @ 'h' => cwrite!(self, bright_purple; f, "{c}"),
+            // System.
+            c @ 's' => cwrite!(self, bright_green; f, "{c}"),
+            // File / No value.
+            c @ '-' => cwrite!(self, bright_black; f, "{c}"),
+            // Directory.
+            c @ 'd' => cwrite!(self, bright_blue; f, "{c}"),
+            // Symbolic link.
+            c @ 'l' => cwrite!(self, bright_cyan; f, "{c}"),
             // Anything else.
             unknown => cwrite!(self, bright_magenta; f, "{unknown}"),
         }
@@ -115,16 +173,6 @@ impl HasColor for PermissionsDisplay<'_> {
 impl Displayer for PermissionsDisplay<'_> {
     fn show<W: Write>(&self, f: &mut W, entry: &Entry) -> Result<()> {
         cwrite!(self, bright_black; f, "[")?;
-
-        if entry.data.is_file() {
-            self.show_char(f, '.')?;
-        } else if entry.data.is_dir() {
-            self.show_char(f, 'd')?;
-        } else if entry.data.is_symlink() {
-            self.show_char(f, 'l')?;
-        } else {
-            self.show_char(f, '-')?;
-        }
 
         self.show_entry(f, entry)?;
 
